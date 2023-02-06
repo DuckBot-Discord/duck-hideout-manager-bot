2ccf694b2b1bd7f6a756952e44ed9f367d122871
Leonardo Cavenago <leonardocavenago05@gmail.com>
2023-02-06 20:06:45 +0100

    Rename flag, and run black on file.

--- cogs/hideout/moderation.py
+++ cogs/hideout/moderation.py
@@ -4,20 +4,28 @@ from typing import Annotated, Optional
 from utils import HideoutCog, HideoutContext, UntilFlag, ShortTime, Timer
 from ._checks import hideout_only, counselor_only
 
+
 class BanFlags(commands.FlagConverter, prefix='--', delimiter=' '):
-    until: Optional[ShortTime]
+    until: Optional[ShortTime] = commands.Flag(name='duration', aliases=['for'])  # type: ignore
+
 
 class Moderation(HideoutCog):
     @hideout_only()
     @counselor_only()
     @commands.command()
-    async def ban(self, ctx: HideoutContext, member: discord.Member, *, reason: Optional[UntilFlag[Annotated[str, commands.clean_content], BanFlags]]):
+    async def ban(
+        self,
+        ctx: HideoutContext,
+        member: discord.Member,
+        *,
+        reason: Optional[UntilFlag[Annotated[str, commands.clean_content], BanFlags]],
+    ):
         if member == ctx.author:
             return await ctx.send("You can't ban yourself!")
 
         if ctx.author.top_role <= member.top_role:
             return await ctx.send("You can't ban someone who has a role higher or equal to yours!")
-        
+
         if ctx.guild.me.top_role <= member.top_role:
             return await ctx.send("I can't ban this member!")
 
@@ -28,18 +36,20 @@ class Moderation(HideoutCog):
                 fmt = ""
             time = reason.flags.until
             if time:
-                #await self.bot.create_timer(time.dt, 'tempban', ctx.guild.id, member.id, precise=False)
+                # await self.bot.create_timer(time.dt, 'tempban', ctx.guild.id, member.id, precise=False)
                 fmt += f"until {discord.utils.format_dt(time.dt, 'R')}"
         else:
             fmt = ""
 
         try:
-            await member.send(f"You have been banned from Duck Hideout{fmt}.\nAppeal this action by emailing `support@duck-bot.com`")
+            await member.send(
+                f"You have been banned from Duck Hideout{fmt}.\nAppeal this action by emailing `support@duck-bot.com`"
+            )
         except discord.HTTPException:
             pass
         await member.ban(reason=f"banned by {ctx.author}{fmt}."[:500])
         await ctx.send(f"Banned {member} {fmt}")
-    
+
     @commands.Cog.listener("on_tempban_time_complete")
     async def on_tempban_time_complete(self, timer: Timer):
         guild_id, member_id = timer.args
@@ -47,7 +57,7 @@ class Moderation(HideoutCog):
             guild = self.bot.get_guild(guild_id)
             if not guild:
                 return
-            
+
             await guild.unban(discord.Object(id=member_id))
 
         except discord.HTTPException:
