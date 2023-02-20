f678ba43284411f259659970f135565e418d3a08
Leonardo Cavenago <leonardocavenago05@gmail.com>
2023-02-20 17:56:59 +0100

    Remove overwritten JSK base command

--- utils/jishaku/__init__.py
+++ utils/jishaku/__init__.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import contextlib
 import io
 import itertools
-import sys
 import time
 import traceback
 from typing import TYPE_CHECKING, Annotated, Any, Optional, TypeVar
@@ -15,11 +14,9 @@ from jishaku.exception_handling import ReplResponseReactor
 from jishaku.features.baseclass import Feature
 from jishaku.features.management import ManagementFeature
 from jishaku.features.python import PythonFeature
-from jishaku.features.root_command import RootCommand
 from jishaku.flags import Flags
 from jishaku.functools import AsyncSender
-from jishaku.math import natural_size
-from jishaku.modules import ExtensionConverter, package_version
+from jishaku.modules import ExtensionConverter
 from jishaku.paginators import PaginatorInterface, WrappedPaginator, use_file_check
 from jishaku.repl import AsyncCodeExecutor, Scope
 from jishaku.repl.repl_builtins import get_var_dict_from_ctx
@@ -28,123 +25,11 @@ from utils.context import HideoutContext
 
 from .. import HideoutCog, add_logging
 
-try:
-    import psutil
-except ImportError:
-    psutil = None
 
 if TYPE_CHECKING:
     from bot import HideoutManager
 
 T = TypeVar("T")
-
-
-class OverwrittenRootCommand(RootCommand):
-    @Feature.Command(
-        parent=None,
-        name="jsk",
-        aliases=["jishaku", "jishacum"],
-        invoke_without_command=True,
-    )
-    async def jsk(self, ctx: HideoutContext):
-        """
-        The Jishaku debug and diagnostic commands.
-
-        This command on its own gives a status brief.
-        All other functionality is within its subcommands.
-        """
-
-        summary = [
-            f"Jishaku v{package_version('jishaku')}, discord.py `{package_version('discord.py')}`, "
-            f"`Python {sys.version}` on `{sys.platform}`".replace("\n", ""),
-            f"Module was loaded <t:{self.load_time.timestamp():.0f}:R>, "
-            f"cog was loaded <t:{self.start_time.timestamp():.0f}:R>.",
-            "",
-        ]
-
-        # detect if [procinfo] feature is installed
-        if psutil:
-            try:
-                proc = psutil.Process()
-
-                with proc.oneshot():
-                    try:
-                        mem = proc.memory_full_info()
-                        summary.append(
-                            f"Using {natural_size(mem.rss)} physical memory and "
-                            f"{natural_size(mem.vms)} virtual memory, "
-                            f"{natural_size(mem.uss)} of which unique to this process."
-                        )
-                    except psutil.AccessDenied:
-                        pass
-
-                    try:
-                        name = proc.name()
-                        pid = proc.pid
-                        thread_count = proc.num_threads()
-
-                        summary.append(f"Running on PID {pid} (`{name}`) with {thread_count} thread(s).")
-                    except psutil.AccessDenied:
-                        pass
-
-                    summary.append("")  # blank line
-            except psutil.AccessDenied:
-                summary.append(
-                    "psutil is installed, but this process does not have high enough access rights "
-                    "to query process information."
-                )
-                summary.append("")  # blank line
-
-        cache_summary = f"{len(self.bot.guilds)} guild(s) and {len(self.bot.users)} user(s)"
-
-        # Show shard settings to summary
-        if isinstance(self.bot, discord.AutoShardedClient):
-            if len(self.bot.shards) > 20:
-                summary.append(
-                    f"This bot is automatically sharded ({len(self.bot.shards)} shards of {self.bot.shard_count})"
-                    f" and can see {cache_summary}."
-                )
-            else:
-                shard_ids = ", ".join(str(i) for i in self.bot.shards.keys())
-                summary.append(
-                    f"This bot is automatically sharded (Shards {shard_ids} of {self.bot.shard_count})"
-                    f" and can see {cache_summary}."
-                )
-        elif self.bot.shard_count:
-            summary.append(
-                f"This bot is manually sharded (Shard {self.bot.shard_id} of {self.bot.shard_count})"
-                f" and can see {cache_summary}."
-            )
-        else:
-            summary.append(f"This bot is not sharded and can see {cache_summary}.")
-
-        # pyright: reportPrivateUsage=false
-        if self.bot._connection.max_messages:
-            message_cache = f"Message cache capped at {self.bot._connection.max_messages}"
-        else:
-            message_cache = "Message cache is disabled"
-
-        if discord.version_info >= (1, 5, 0):
-            presence_intent = f"presence intent is {'enabled' if self.bot.intents.presences else 'disabled'}"
-            members_intent = f"members intent is {'enabled' if self.bot.intents.members else 'disabled'}"
-
-            summary.append(f"{message_cache}, {presence_intent} and {members_intent}.")
-        else:
-            guild_subscriptions = f"guild subscriptions are {'enabled' if self.bot._connection.guild_subscriptions else 'disabled'}"  # type: ignore
-
-            summary.append(f"{message_cache} and {guild_subscriptions}.")
-
-        # pyright: reportPrivateUsage=strict
-
-        # Show websocket latency in milliseconds
-        summary.append(f"Average websocket latency: {round(self.bot.latency * 1000, 2)}ms")
-
-        summ = "\n".join(summary)
-        if ctx.channel.permissions_for(ctx.me).embed_links:  # type: ignore
-            embed = discord.Embed(description=summ, color=ctx.bot.colour)
-            await ctx.send(embed=embed)
-        else:
-            await ctx.send(summ)
 
 
 class OverwrittenManagementFeature(ManagementFeature):
@@ -187,17 +72,11 @@ class OverwrittenManagementFeature(ManagementFeature):
             await ctx.send(page)
 
 
-features = list(STANDARD_FEATURES)
-features.remove(RootCommand)
-features.append(OverwrittenRootCommand)
-features.remove(ManagementFeature)
-features.append(OverwrittenManagementFeature)
-
-
 class HideoutManagerJishaku(
     HideoutCog,
-    *features,
-    *OPTIONAL_FEATURES,
+    OverwrittenManagementFeature,
+    *STANDARD_FEATURES,  # type: ignore
+    *OPTIONAL_FEATURES,  # type: ignore
 ):
     """
     The main frontend class for JIshaku.
@@ -306,7 +185,7 @@ class HideoutManagerJishaku(
 
         try:
             async with ReplResponseReactor(ctx.message):
-                with self.submit(ctx):  # type: ignore
+                with self.submit(ctx):
                     with contextlib.redirect_stdout(printed):
                         executor = AsyncCodeExecutor(argument.content, scope, arg_dict=arg_dict)
                         start = time.perf_counter()
