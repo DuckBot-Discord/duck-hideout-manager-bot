15f8e09a2f5dc2438f327c030eb0ea481b287b9b
irregularunit <pawsfiction@gmail.com>
2023-03-25 01:02:25 +0100

    100% Typing? Or not I tried...

--- utils/async_utils.py
+++ utils/async_utils.py
@@ -1,65 +1,300 @@
-import inspect
+from __future__ import annotations
+
+import asyncio
+import logging
 from abc import ABCMeta
-from typing import Any, Callable, Dict, Tuple, Type, TypeVar
+from contextlib import suppress
+from typing import Any, Callable, Coroutine, Generator, MutableSet, Optional, overload
+from weakref import WeakSet
 
 __all__: tuple[str, ...] = ("AsyncInstance",)
 
 
-_T = TypeVar("_T")
+_log: logging.Logger = logging.getLogger(__name__)
+CloseableType = Callable[[], Any | Coroutine[Any, Any, Any]]
+
+
+class Task:
+    """A custom task store for asyncio tasks and futures.
+
+    Parameters
+    ----------
+    loop: :class:`asyncio.AbstractEventLoop`
+        The event loop to use for creating tasks and futures.
+
+    Attributes
+    ----------
+    tasks: :class:`set`
+        A set of all tasks created by this store.
+    futures: :class:`set`
+        A set of all futures created by this store.
+    children: :class:`set`
+        A set of all child stores created by this store.
+    close_callbacks: :class:`set`
+        A set of all close callbacks added to this store.
+    is_closed: :class:`bool`
+        Whether or not this store is closed.
+    """
+
+    def __init__(self, loop: asyncio.AbstractEventLoop):
+        self.tasks: MutableSet[asyncio.Task[Any]] = WeakSet()
+        self.futures: MutableSet[asyncio.Future[Any]] = WeakSet()
+        self.children: MutableSet[Task] = WeakSet()
+        self.close_callbacks: set[CloseableType] = set()
+        self.__loop: asyncio.AbstractEventLoop = loop
+        self.__closing: asyncio.Future[bool] = self.__loop.create_future()
+
+    def get_child(self) -> Task:
+        # This is a bit of a hacky to get around the task that
+        # but it's the best way I can think of to do it.
+        store = self.__class__(self.__loop)
+        self.children.add(store)
+        return store
+
+    def add_close_callback(self, func: CloseableType) -> None:
+        self.close_callbacks.add(func)
+
+    def compose_task(self, *args: Any, **kwargs: Any) -> asyncio.Task[Any]:
+        task = self.__loop.create_task(*args, **kwargs)
+        self.tasks.add(task)
+        task.add_done_callback(self.tasks.remove)
+        return task
+
+    def compose_future(self) -> asyncio.Future[Any]:
+        future = self.__loop.create_future()
+        self.futures.add(future)
+        future.add_done_callback(self.futures.remove)
+        return future
+
+    @property
+    def is_closed(self) -> bool:
+        return self.__closing.done()
+
+    async def close(self, exc: Optional[Exception] = None) -> None:
+        if self.__closing.done():
+            return
+
+        if exc is None:
+            self.__closing.set_result(True)
+        else:
+            self.__closing.set_exception(exc)
+
+        for future in self.futures:
+            if future.done():
+                continue
+
+            future.set_exception(
+                exc or asyncio.CancelledError("Object %r closed" % self),
+            )
+
+        tasks: list[asyncio.Task[Any] | Coroutine[Any, Any, Any]] = []
+
+        for func in self.close_callbacks:
+            try:
+                result: Any | Coroutine[Any, Any, Any] = func()
+            except BaseException:
+                # Something gone bad happened while trying to close
+                # the object. We need to log this and continue.
+                _log.exception(
+                    (
+                        "Error while trying to close %r. "
+                        "The exception was not retrieved due to an error in "
+                        "asyncio's exception handler. ",
+                        func,
+                    ),
+                    self,
+                )
+                continue
+
+            if asyncio.iscoroutine(result) or isinstance(result, asyncio.Future):
+                # Simply add our coros to the list of tasks to wait on.
+                tasks.append(result)  # type: ignore
+
+        for task in self.tasks:
+            if task.done():
+                continue
+
+            task.cancel()
+            tasks.append(task)
+
+        for store in self.children:
+            tasks.append(store.close())
+
+        await asyncio.gather(*tasks, return_exceptions=True)
 
 
 class AsyncABCMeta(ABCMeta):
-    def __init__(cls, name: str, bases: Tuple[Type[Any], ...], methods: Dict[str, Any]) -> None:
-        coros: Dict[str, Callable[..., Any]] = {}
-        for base in reversed(cls.__mro__):
-            coros.update((name, val) for name, val in vars(base).items() if inspect.iscoroutinefunction(val))
+    """
+    This metaclass ensures that the ``__ainit__`` method is a coroutine.
+    """
+    def __new__(
+        cls,
+        clsname: str,
+        bases: tuple[type, ...],
+        namespace: dict[str, Any],
+    ) -> AsyncABCMeta:
+        instance = super(AsyncABCMeta, cls).__new__(
+            cls,
+            clsname,
+            bases,
+            namespace,
+        )
 
-        for name, val in vars(cls).items():
-            if name in coros and not inspect.iscoroutinefunction(val):
-                raise TypeError('Must use async def %s%s' % (name, inspect.signature(val)))
-        super().__init__(name, bases, methods)
+        if not asyncio.iscoroutinefunction(instance.__ainit__):  # type: ignore
+            raise TypeError("__ainit__ must be coroutine")
+
+        return instance
 
 
-class AsyncABC(metaclass=AsyncABCMeta):
-    pass
+class AsyncInstanceType(metaclass=AsyncABCMeta):
+    """
+    Parameters
+    ----------
+    *args: Any
+        The arguments to pass to ``__ainit__``.
+    **kwargs: Any
+        The keyword arguments to pass to ``__ainit__``.
 
+    Attributes
+    ----------
+    loop: :class:`asyncio.AbstractEventLoop`
+        The event loop to use for creating tasks and futures.
+    """
+    __slots__: tuple[str, ...] = ("_args", "_kwargs")
+    _args: tuple[Any, ...]
+    _kwargs: dict[str, Any]
 
-class AsyncInstanceType(AsyncABCMeta):
-    @staticmethod
-    def __new__(cls: Type[_T], clsname: str, bases: Tuple[Type[Any], ...], attributes: Dict[str, Any]) -> _T:  # type: ignore
-        if '__init__' in attributes and not inspect.iscoroutinefunction(attributes['__init__']):
-            raise TypeError('__init__ must be a coroutine')
-        return super().__new__(cls, clsname, bases, attributes)
+    def __new__(
+        cls,
+        *args: Any,
+        **kwargs: Any,
+    ) -> AsyncInstanceType:
+        instance = super().__new__(cls)
+        instance._args = args
+        instance._kwargs = kwargs
+        return instance
 
-    async def __call__(cls: Type[_T], *args: Any, **kwargs: Any) -> _T:
-        self = cls.__new__(cls, *args, **kwargs)
+    @property
+    def loop(self) -> asyncio.AbstractEventLoop:
+        return asyncio.get_running_loop()
 
-        await self.__init__(*args, **kwargs)  # type: ignore
+    def __await__(self) -> Generator[Any, None, "AsyncInstanceType"]:
+        yield from self.__ainit__(*self._args, **self._kwargs).__await__()
+
+        # blep :<
         return self
 
+    async def __ainit__(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        ...
 
-class AsyncInstance(metaclass=AsyncInstanceType):
-    """A class that creates asynchronous instances.
 
-    Its a base class for classes that need to be asynchronous.
+class AsyncInstance(AsyncInstanceType):
+    """A base class for creating async classes.
 
-    Example
-    -------
-
-    >>> import asyncio
-    >>> from utils import AsyncInstance
-    ...
-    >>> class TestClass(AsyncInstance):
-    ...     # Note creating this constructor with `def` will raise an error.
-    ...     async def __init__(self, name: str):
-    ...         self.name = name
-    ...
-    ...
-    >>> async def main():
-    ...     test = await TestClass("test")
-    ...     print(test.name)
-    ...     await asyncio.get_running_loop().shutdown_asyncgens()
-    ...
-    >>> asyncio.run(main())
+    This class is designed to be used as a base class for creating
+    async classes. It provides a ``__await__`` method which calls
+    ``__ainit__`` and returns the instance.
     """
-    pass
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        self.__closed = False
+        self._async_class_task_store: Task
+
+    @property
+    def __tasks__(self) -> Task:
+        return self._async_class_task_store
+
+    @property
+    def is_closed(self) -> bool:
+        return self.__closed
+
+    def compose_task(self, *args: Any, **kwargs: Any) -> asyncio.Task[Any]:
+        return self.__tasks__.compose_task(*args, **kwargs)
+
+    def compose_future(self) -> asyncio.Future[Any]:
+        return self.__tasks__.compose_future()
+
+    async def __adel__(self) -> None:
+        pass
+
+    def __await__(self) -> Generator[Any, None, AsyncInstance]:
+        if not hasattr(self, "_async_class_task_store"):
+            self._async_class_task_store = Task(self.loop)
+
+        yield from self.compose_task(
+            self.__ainit__(*self._args, **self._kwargs),
+        ).__await__()  # cursed
+        return self
+
+    def __del__(self) -> None:
+        if self.__closed:
+            return
+
+        with suppress(BaseException):
+            self.loop.create_task(self.close())
+
+    async def close(self, exc: Optional[Exception] = None) -> None:
+        if self.__closed:
+            return
+
+        tasks: list[asyncio.Task[Any] | Coroutine[Any, Any, Any]] = []
+
+        if hasattr(self, "_async_class_task_store"):
+            # Allows for graceful closing of the task manager.
+            tasks.append(self.__adel__())
+            tasks.append(self.__tasks__.close(exc))
+            self.__closed = True
+
+        if not tasks:
+            return
+
+        # Gather all the tasks and wait for them to finish.
+        # Could use the new syntax but I am not sure if every contributor
+        # is using 3.11 yet. Changable in the future.
+        await asyncio.gather(*tasks, return_exceptions=True)
+
+    # I just looked up the overload docs and I am still confused
+    # hope this works, as plannend :p
+    @overload
+    async def __ainit__(self) -> None:
+        ...
+
+    @overload
+    async def __ainit__(self, *args: Any, **kwargs: Any) -> None:
+        ...
+
+    async def __ainit__(self, *args: Any, **kwargs: Any) -> None:
+        ...
+
+
+if __name__ == "__main__":
+    # NOTE: DELETE THIS BEFORE MERGING
+    async def compose_test_instance() -> None:
+        class Test(AsyncInstance):
+            def __init__(self, *args: Any, **kwargs: Any) -> None:
+                super().__init__(*args, **kwargs)
+
+            async def __ainit__(self, *args: Any, **kwargs: Any) -> None:
+                print("Test.__ainit__", args, kwargs)
+                await asyncio.sleep(1)
+                print("Test.__ainit__ done")
+
+            async def __adel__(self) -> None:
+                print("Test.__adel__")
+                await asyncio.sleep(1)
+                print("Test.__adel__ done")
+
+        test = await Test(1, 2, 3, a=4, b=5, c=6)
+        print("Test.__await__ done")
+        await test.close()
+
+    # Test.__ainit__ (1, 2, 3) {'a': 4, 'b': 5, 'c': 6}
+    # Test.__ainit__ done
+    # Test.__await__ done
+    # Test.__adel__
+    # Test.__adel__ done
+
+    asyncio.run(compose_test_instance())
