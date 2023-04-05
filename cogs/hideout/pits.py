dcb5bdc610cf886ea3321a13d1bf736fdcdab76e
Leonardo Cavenago <leonardocavenago05@gmail.com>
2023-04-05 23:12:35 +0200

    Use no_auto attr to prevent automatic actions

--- cogs/hideout/pits.py
+++ cogs/hideout/pits.py
@@ -353,6 +353,9 @@ class PitsManagement(HideoutCog):
     @commands.Cog.listener('on_member_join')
     async def block_handler(self, member: discord.Member):
         """Blocks a user from your channel."""
+        if self.no_auto:
+            return
+
         guild = member.guild
 
         channel_ids = await self.bot.pool.fetch(
@@ -395,6 +398,9 @@ class PitsManagement(HideoutCog):
     @commands.Cog.listener('on_tempblock_timer_complete')
     async def on_tempblock_timer_complete(self, timer: Timer):
         """Automatic temp block expire handler"""
+        if self.no_auto:
+            return
+
         guild_id, channel_id, user_id, author_id = timer.args
 
         try:
@@ -434,6 +440,8 @@ class PitsManagement(HideoutCog):
     @commands.Cog.listener('on_member_remove')
     async def pit_auto_archive(self, member: discord.Member):
         """Automatically archives pits that are not used."""
+        if self.no_auto:
+            return
 
         pit_id: int | None = await self.bot.pool.fetchval('''SELECT pit_id FROM pits WHERE pit_owner = $1''', member.id)
         if pit_id is None:
@@ -471,6 +479,8 @@ class PitsManagement(HideoutCog):
     @commands.Cog.listener('on_member_join')
     async def pit_auto_unarchive(self, member: discord.Member):
         """Automatically archives pits that are not used."""
+        if self.no_auto:
+            return
 
         record = await self.bot.pool.fetchrow('''SELECT * FROM pits WHERE pit_owner = $1''', member.id)
 
